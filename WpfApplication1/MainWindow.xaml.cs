717c8f9f5b6624975817497ea5652fd0dcc4e004
Scrabbi <bructal@gmail.com>
2020-08-08 09:17:08 +0300

    First try 08082020

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+
+namespace WpfApplication1
+{
+  /// <summary>
+  /// Логика взаимодействия для MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+      private CancellationTokenSource _tokenSourse;
+      private Network _network;
+    /// <summary>
+    /// Сокет создавайемый, после принятия данных
+    /// </summary>
+    Socket handler;
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+    
+    
+    private async void  Start_Serv_Click(object sender, RoutedEventArgs e)
+    {
+            _network=new Network();
+                //От перекликивания
+            Start_Serv.IsEnabled=false;
+            
+            _tokenSourse=new CancellationTokenSource();
+            CancellationToken token=_tokenSourse.Token;
+
+                              //Стоим на прослушивании
+            //try
+            //{
+                string ip=TexBox_IP . Text ;
+                string port=TexBox_port . Text ;
+
+                Task<Socket> task_wait = Task<Socket> . Factory . StartNew ( ( ) => _network . WaitRequest ( ip, port) );
+                handler= await task_wait;
+            //}
+            //catch ( Exception ex )
+            //{
+                //    MessageBox.Show(string . Format ( "ПРоизошла ошибка: {0}" , ex . Message ));
+            //}
+            // Мы дождались клиента, пытающегося с нами соединиться. Получаем ключ -- сообщение. Оно у нас посто одно число "224"
+                try 
+	                {
+                       Task task = Task . Factory . StartNew ( ( ) => _network . BeginBroadcasting ( handler , token ) , token );
+                        await task;
+	                }
+	             catch (Exception ex)
+	                {
+                        MessageBox.Show(string . Format("Произошла ошибка!!!_, {0}", ex));
+	                }
+        }
+
+    private void End_Serv_Click(object sender, RoutedEventArgs e)
+    {
+        if ( handler!=null )
+        {
+            _tokenSourse . Cancel ( );
+            handler . Shutdown ( SocketShutdown . Both );
+            handler . Close ( );
+            Start_Serv . IsEnabled=true;
+        }
+        else
+        MessageBox.Show("Закрывать нечего, не было принятых данных");
+   }
+  }
+}
---
+++ WpfApplication1/Network.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System . Collections . Generic;
+using System . Linq;
+using System . Text;
+using System . Threading . Tasks;
+using System . Threading;
+using System . Net;
+using System . Net . Sockets;
+
+namespace WpfApplication1
+{
+    public  class Network
+    {
+        /// <summary>
+        /// Ожидание запроса на трансляцию 
+        /// </summary>
+        /// <param name="IP"></param>
+        /// <param name="Port"></param>
+        public  Socket WaitRequest ( string IP , string Port )
+        {
+            //Локальную конечную точку
+            IPEndPoint ipPoint = new IPEndPoint ( IPAddress . Parse ( IP ) , Convert . ToInt32 ( Port ) );
+            // Создаем сокет Tcp/Ip
+            Socket listenSocket = new Socket ( AddressFamily . InterNetwork , SocketType . Stream , ProtocolType . Tcp );
+            // Связываем сокет с локальной точкой, по которой будем принимать данные
+            listenSocket .Bind ( ipPoint );
+            // Начинаем прослушивание
+            listenSocket . Listen (1000 );   
+           return listenSocket . Accept ( ) ;
+        }
+        /// <summary>
+        /// Бесконечный цикл передачи данных.
+        /// </summary>
+        /// <param name="handler"></param>
+        public  void BeginBroadcasting ( Socket handler , CancellationToken token )
+        {
+            byte[] key = new byte [ 1 ]; // Буфер для получаемых данных -- ключа. Получаемые байты
+            int bytes = handler . Receive ( key );// Количество полученных байтов
+
+            if ( key [ 0 ] == 224 )
+            while(true)
+            { 
+                        // Ловля ошибки или остановки приложения
+                     token . ThrowIfCancellationRequested ( );
+                        // Готовим ответ. Всего должно 43 байта посылаться в форме массива.
+                    byte[] dataSent = new byte [ 43] ;
+                        // Заполним Fcurrent1 -- это типа частота от прибора, дающего ток.
+                    byte[] Fcurrent1 = new byte [ 4 ];
+
+                        // Канал 1 отошлет от 1 до 5 число 
+                    Random rand = new Random ( );
+                    Single d1 = rand . Next ( 1 , 5 );
+                    d1 *=  25000; // Хотим в итоге видеть цифры тока
+                    Fcurrent1 = BitConverter . GetBytes ( d1 );
+                    //Записываем в посылку Fcurrent1
+                    for ( int i = 0 ; i < Fcurrent1 . Length ; i++ )
+                    {
+                        dataSent [ i + 1 ] = Fcurrent1 [ i ];
+                    }
+                        //Заполним Power1 -- степень тока
+
+                    dataSent [ 9 ] = 8; //1..5*10-8
+
+
+                         //Заполним для ворго канала все то же. Канал 1 отошлет от 6 до 9 число 
+                    byte[] Fcurrent2 = new byte [ 4 ];
+                    Single d2 = rand . Next ( 6 , 9 );
+                    d2 *=  25000;//Хотим в итоге видеть 11..20*10-8 ток
+                    Fcurrent2 = BitConverter . GetBytes ( d2 );
+                    for ( int i = 0 ; i < Fcurrent2 . Length ; i++ )
+                    {
+                        dataSent [ i + 15 ] = Fcurrent2 [ i ];
+                    }
+                        //Заполним Power2
+                    dataSent [ 23 ] = 8;
+
+                        //Послыаем
+
+                    handler . Send ( dataSent );
+                            //Задержка отправки
+                    //Thread . Sleep ( 1000 );
+            }
+        }
+        
+    }
+}
